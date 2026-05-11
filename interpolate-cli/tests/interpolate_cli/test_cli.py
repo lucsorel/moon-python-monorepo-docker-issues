13330d381e9baa875c117d8d99461d24e0fd2bfb
Luc Sorel-Giffo <luc.sorel@gmail.com>
2026-05-11 17:28:44 +0200

    test(cli, cli_dep): add integration test

---
+++ interpolate-cli/tests/interpolate_cli/test_cli.py
@@ -0,0 +1,8 @@
+from click.testing import CliRunner
+from interpolate_cli import main
+
+def test_interpolate_cli():
+    runner = CliRunner()
+    result = runner.invoke(main, ['--value', '0.5', '--output-min', '10.0', '--output-max', '20.0'])
+    assert result.exit_code == 0
+    assert result.output == '15.0\n'
